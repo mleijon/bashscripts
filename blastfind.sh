d7b8a447528d51a8fc5a90d5ca4989824069fa5a
mleijon <mikael.leijon@gmail.com>
2019-05-14 14:26:27 +0200

    commit

---
+++ blastfind.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+FILES=$PWD/*.blastn
+nr=0
+nr_of_files=$(ls *.blastn|wc -l)
+for b in ${FILES}; do
+    nr=$((nr+1))
+    echo -en 'Processing file: '${nr}'/'${nr_of_files}\\r
+    f=$(dirname "$b")
+    f=$(dirname "$f")/$(basename "$b")
+    f=${f/blastn/fa}
+    blast_find.py -f ${f} -b ${b} -t 'virus';wait
+done
+mkdir top;mkdir deep;mkdir virus_fasta;wait
+mv *top.txt top; mv *deep.txt deep; mv *virus.fa virus_fasta
