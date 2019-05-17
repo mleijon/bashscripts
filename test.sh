dc124128e0119626ff2d5a24bda518b2ea07079b
mleijon <mikael.leijon@gmail.com>
2019-05-17 17:14:24 +0200

    testfil

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-dirs="/home/bo $HOME"
-for f in  $dirs ; do
-ls $f
-echo $'\n\n\n'
+dirs="/ngs/mikael/CLINF/2*/"
+for d in  $dirs ; do
+scp -r ${d}/ decypher@172.17.1.169:micke/data/clinf/reindeer/$(basename ${d})/;wait
 done
