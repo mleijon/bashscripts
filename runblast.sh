ac7fe523b8ead434c07d5ca6b84897281145a432
mleijon <mikael.leijon@gmail.com>
2019-07-01 22:31:11 +0200

    runblast

--- runblast.sh
+++ runblast.sh
@@ -7,7 +7,7 @@ for f in ${FILES}; do
     nr=$((nr+1))
     echo -en 'Processing file: '${nr}'/'${nr_of_files}\\r
     blastn -task blastn -query ${f} -db /home/micke/blast_dbs/nt \
-    -out $PWD/$(basename "$f").blastn -evalue 1e-5 -word_size 11 -gapopen 5\
+    -out ${f/fa/blastn} -evalue 1e-5 -word_size 11 -gapopen 5\
     -gapextend 2 -reward 2 -penalty -3 -num_descriptions 5 -num_alignments 0\
     -outfmt 0 -num_threads 8 -parse_deflines
 done
