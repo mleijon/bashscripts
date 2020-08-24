cc445ba829d6894286805898501630c5f0dc077a
micke <mikael.leijon@gmail.com>
2020-08-24 13:51:52 +0200

    testfil

--- scp2dec.sh
+++ scp2dec.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 dirs="/ngs/mikael/CLINF/2*/"
 for d in  $dirs ; do
-scp -r ${d}/ decypher@172.17.1.169:micke/data/clinf/reindeer/$(basename ${d})/;wait
+scp -r ${d}/ decypher@172.17.1.169:micke/data/clinf/reindeer/"$(basename ${d})"/;wait
 done
