552ee5c153a1143dca7d83e6f4c2753a302fa16f
micke <mikael.leijon@gmail.com>
2020-08-24 13:51:03 +0200

    testfil

--- blastfind.sh
+++ blastfind.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-FILES="$PWD/*.blastn"
+FILES="$PWD/*.$1"
 nr=0
-nr_of_files=$(find -- *.blastn|wc -l)
+nr_of_files=$(find -- *.$1|wc -l)
 for b in ${FILES}; do
     nr=$((nr+1))
     echo -en 'Processing file: '${nr}'/'"${nr_of_files}"\\r
     f=$(dirname "$b")
     f=$(dirname "$f")/$(basename "$b")
-    f=${f/blastn/fa}
+    f=${f/$1/fa}
     blast_find.py -f "${f}" -b "${b}" -t 'virus';wait
 done
-mkdir top;mkdir deep;mkdir virus_fasta;wait
+mkdir -p top;mkdir -p deep;mkdir -p virus_fasta;wait
 mv -- *top.txt top; mv -- *deep.txt deep; mv -- *virus.fa virus_fasta
+wait; gzip *.$1
