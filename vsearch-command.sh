5fbdc0301423275b7c8d14b40664b21963d0f7e3
mleijon <mikael.leijon@gmail.com>
2020-11-09 00:22:39 +0100

    vsearch

--- vsearch-command.sh
+++ vsearch-command.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 vsearch --fastq_join *R1_trunc.fastq --reverse *R2_trunc.fastq --fastqout /dev/stdout|vsearch --derep_fulllength - --output testjoin_derep.fastq --sizeout --log log.txt
 
