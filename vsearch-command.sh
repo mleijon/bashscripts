787e2bf41f3bd982ab2e2a971b59411c97029e12
micke <mikael.leijon@gmail.com>
2020-11-09 17:14:53 +0100

    fungerar

--- vsearch-command.sh
+++ vsearch-command.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-FILES="/media/micke/extra/reningsverksdata/00-RawData-k/*R1*"
+FILES="/storage/MSBvatten/msbvattenJS/SE-2194/190822_A00181_0108_AHLFMYDSXX/Sample_SE-2194-Botvid-Granby-filter/*R1*"
 for f in $FILES; do
-  vsearch --fastq_join "$f" --reverse "${f/R1/R2}" --log log-join.txt --no_progress --fastqout /dev/stdout|\
-  vsearch --derep_fulllength - --output /dev/stdout --sizeout --no_progress --log log-derep.txt|\
- /home/micke/PycharmProjects/fasta-tools/vsearchjoin_split.py| gzip > test.fa.gz
+  outname=${f##*/}
+  outname=${outname%%_*}
+  outdir=/storage/micke/msbh2o/out_diamond
+  vsearch --fastq_join "$f" --reverse "${f/R1/R2}" --log $outdir/$outname-join.log --no_progress --fastqout /dev/stdout|\
+  vsearch --derep_fulllength - --output /dev/stdout --sizeout --no_progress --log $outdir/$outname-derep.log|\
+  /home/micke/PycharmProjects/fasta-tools/vsearchjoin_split.py|\
+  diamond blastx -d /ssd2/diamondDB/nr -q "$f" -o $outdir/$outname.daa -k5 -e1E-5 -f102 -b20 -c1 --compress 1;wait
 done
