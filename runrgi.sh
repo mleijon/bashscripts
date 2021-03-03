dd4823058b9778f75e86605ea2bf4378a16ebbcd
mleijon <mikael.leijon@gmail.com>
2021-03-03 22:47:13 +0100

    :-)

---
+++ runrgi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FILES="/home/micke/unicycler-run2/assemblies_renamed/*.fasta"
+for f in $FILES; do
+outname=${f##*/}
+echo processing: $outname
+outname=${outname%%.fasta*}
+wait;rgi main -i $f -o $outname -t contig --exclude_nudge --clean;wait
+done
