2225abc7aa7c1ddee17f1a08e791f175470fccce
mleijon <36968756+mleijon@users.noreply.github.com>
2023-11-23 23:41:48 +0100

    vip

--- extseqfa.sh
+++ extseqfa.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-nr_rows=$(sed '/^$/d' $1|wc -l)
+tmpfile=$(mktemp)
+sed '/^>/s/$/@@/' "$1"|sed -z 's/\n//g'|sed 's/@@/\n/g'|sed 's/>/\n>/g'|sed '/^$/d' > "$tmpfile"
+nr_rows=$(sed '/^$/d' "$tmpfile"|wc -l)
 nr_seqs=$((nr_rows/2))
-if [ $2 -gt $nr_seqs ]; then
+if [ "$2" -gt $nr_seqs ]; then
 	echo "Only $nr_seqs sequences."
 	exit
 fi
-sed '/^$/d' $1|tail -n $((2*(nr_seqs-$2+1)))|head -2 
-
-
-#sed -r "s/(.{5})/\1\\n/g"
+dseq=$(tail -n $((2*(nr_seqs-"$2"+1))) "$tmpfile"|head -2|tail -1)
+dseq+=$(seq -s@ $((1+"$2"-${#dseq}%"$2"))|tr -d "[:digit:]")
+echo "$dseq"|sed -r "s/(.{$3})/>split\n\1\\n/g"|tr -d "@"|\
+awk '{sub("split", "split_" ++i); print; if (substr($1,1,1) != ">") --i}' -
