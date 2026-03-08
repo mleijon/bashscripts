45ef391e7b4a6ef9ff74a86bf9269fb8674f4116
mikael.leijon <mikael.leijon@gmail.com>
2026-03-08 21:11:35 +0100

    Updated for safety

--- ariba_run.sh
+++ ariba_run.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
-dirs1="/ngs/mikael/S_Aureus/KA/Master_Thesis_Emily_Atkins/run2/1*"
-dirs2="/ngs/mikael/S_Aureus/KA/Master_Thesis_Emily_Atkins/run2/Ma*"
-for d in  $dirs1 ; do
-#wait; ariba run /home/mikael/ariba/card "${d}"/*_1.fastq.gz "${d}"/*_2.fastq.gz "${d}"/card
-wait; ariba run /ngs/mikael/S_Aureus/KA/Master_Thesis_Emily_Atkins/run2/VF.out "${d}"/*_1.fastq.gz "${d}"/*_2.fastq.gz "${d}"/results-VF
-done
-for d in  $dirs2 ; do
-#wait; ariba run /home/mikael/ariba/card "${d}"/*_1.fastq.gz "${d}"/*_2.fastq.gz "${d}"/card
-wait; ariba run /ngs/mikael/S_Aureus/KA/Master_Thesis_Emily_Atkins/run2/VF.out "${d}"/*_1.fastq.gz "${d}"/*_2.fastq.gz "${d}"/results-VF
-done
+set -euo pipefail
 
+if [[ "${CONDA_DEFAULT_ENV:-}" != "ariba" ]]; then
+    echo "Error: Conda environment 'ariba' is not active." >&2
+    exit 1
+fi
+
+base_dir="/ngs/mikael/S_Aureus/KA/Master_Thesis_Emily_Atkins/run2"
+
+for d in "$base_dir"/1* "$base_dir"/Ma*; do
+  if [ -d "$d" ]; then
+    ariba run "$base_dir"/VF.out "${d}"/*_1.fastq.gz "${d}"/*_2.fastq.gz "${d}"/results-VF
+  fi
+done
