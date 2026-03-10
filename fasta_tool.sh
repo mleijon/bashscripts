19e9fd6ba3c06234f95e6ec4f1dbaadb2a9f3a6b
mikael.leijon <mikael.leijon@gmail.com>
2026-03-10 09:43:49 +0100

    updates

--- fasta_tool.sh
+++ fasta_tool.sh
@@ -1,125 +1,103 @@
 #!/bin/bash
+# Safety: exit on error or undefined variables
+set -ueo pipefail
 
-clear
-
-Help()
-{
-   # Display Help
-   echo
-   echo "##########################################HELP###############################################"
-   echo "# 'fasta_tools.sh' with the flag '-s' splits an input fasta file (arg. 1) into a user       #"
-   echo "# defined number (arg. 2) of approx. equally sized files without breaking any fasta records #"
-   echo "# between files. Optionally the prefix for the output files can be given (arg. 3), which    #"
-   echo "# otherwise defaults to: 'split_fa'. With the flag '-e' a number of fasta sequence records  #"
-   echo "# (arg 2) will be extracted from the input file (arg. 1). The arguments arg. 1-3 must be    #"
-   echo "#  given after the flags (-s or -e). The maximum number of splits is 99.                    #"
-   echo "#                                                                                           #"
-   echo "# Syntax: fasta_split.sh [-s] [-e] [-h] arg1 arg2 [arg3]                                    #"
-   echo "# options:                                                                                  #"
-   echo "# -h     Print this Help.                                                                   #"
-   echo "# -s     Splits fasta file (arg. 1) into a given (arg. 2) number of files. An output file   #"   
-   echo "#        prefix (arg. 3) may optionally be given.                                           #"
-   echo "# -e     Extract a number (arg. 2) of sequence records from the input fasta file (arg. 1)   #"
-   echo "#############################################################################################"
-   echo
-   read -n 1 -s
+Help() {
+   echo "################################################################################"
+   echo "# fasta_tool.sh: Robust FASTA splitting and extraction                         #"
+   echo "#                                                                              #"
+   echo "# Syntax: fasta_tool.sh [-s | -e] <input_file> <number> [prefix]               #"
+   echo "# options:                                                                     #"
+   echo "# -s     Split file into <number> of roughly equal parts (records).            #"
+   echo "# -e     Extract first <number> of records from the file.                      #"
+   echo "# -h     Print this Help.                                                      #"
+   echo "################################################################################"
 }
 
-split_file()
-{
-   if [ "$2" -gt  99 ]; then
-      echo "Maximally 99 splits."; exit 1
-   fi
-   if [ $# -eq 2 ]; then
-      prefix="split_fa"
-   else
-      prefix=$3
-   fi
-   for f in "$PWD/""$prefix"*;do
-     rm -f "$f"
-   done
-   rec_cnt=$(grep -c '>' "$1")
-   recs_file=$(awk -v a="$rec_cnt" -v b="$2" 'BEGIN{print int(a/b)}')
-   split -dt'>' -a2 -l"$recs_file" --additional-suffix=.fasta "$1" "$prefix"
-   files=$PWD/$prefix"*"
-   for f in $files; do
-      sed '/^>$/d' "$f"|
-      awk '{ if ( substr($1,1,1) != ">" && NR == 1 ) print ">"$0; else print $0;}' - > "${f/$prefix/_$prefix}"
-      mv "${f/$prefix/_$prefix}" "$f"
-   done
-   if [ "$2" -lt 10 ]; then
-      cat "$f" >> "$(awk -v file="$f" -v nr="$2" 'BEGIN{sub(/.\.fasta/, nr - 1 ".fasta", file); print file}')"
-   elif [ "$2" -eq 10 ]; then
-      cat "$f" >> "$prefix""09.fasta"
-   else
-      cat "$f" >> "$(awk -v file="$f" -v nr="$2" 'BEGIN{sub(/..\.fasta/, nr - 1 ".fasta", file); print file}')"
-   fi
-   rm "$f"
-   echo -e "Splitting completed!"
+split_file() {
+    local input="$1"
+    local num_splits="$2"
+    local prefix="${3:-split_fa}"
+
+    if [[ "$num_splits" -lt 1 ]]; then echo "Error: Splits must be > 0"; exit 1; fi
+
+    echo "📊 Counting records in $input..."
+    local total_recs=$(grep -c '>' "$input")
+    local recs_per_file=$(( (total_recs + num_splits - 1) / num_splits ))
+
+    echo "✂️ Splitting $total_recs records into $num_splits files (~$recs_per_file records each)..."
+
+    # Use awk for record-aware splitting (prevents broken records)
+    awk -v recs_per_file="$recs_per_file" -v prefix="$prefix" '
+        BEGIN {RS=">"; ORS=""}
+        NR > 1 {
+            if ((NR-2) % recs_per_file == 0) {
+                file_idx++;
+                out_file = sprintf("%s_%02d.fasta", prefix, file_idx);
+            }
+            print ">"$0 > out_file
+        }
+    ' "$input"
+
+    echo "✅ Done! Created $(ls ${prefix}_*.fasta | wc -l) split files."
 }
 
-extract_seqs()
-{
-if [ "$2" -ge "$(grep -c '>' < "$1")" ]; then
-   echo "Nr of sequence records in fasta file are less or equal to $2. No splitting."
-   exit 0
-fi
-csplit -zs "$1" /"$(grep -m$(( $2 + 1 )) '>' "$1" |tail -n1)"/
-mv xx00 "$(awk -v file="$1" -v nr="$2" 'BEGIN{sub(/\.fasta/, "_" nr ".fasta" , file); print file}')"
-rm xx01
-echo "Extraction completed!"
+extract_seqs() {
+    local input="$1"
+    local num_recs="$2"
+    local prefix="${3:-extracted}"
+
+    echo "📦 Extracting first $num_recs records from $input..."
+
+    # Use awk to extract records safely
+    awk -v limit="$num_recs" -v prefix="$prefix" '
+        BEGIN {RS=">"; ORS=""; count=0}
+        NR > 1 && count < limit {
+            print ">"$0 > (prefix ".fasta");
+            count++;
+        }
+        NR > 1 && count >= limit {
+            print ">"$0 > (prefix ".remainder.fasta");
+        }
+    ' "$input"
+
+    echo "✅ Extraction completed: ${prefix}.fasta"
+    echo "📝 Remaining sequences saved to: ${prefix}.remainder.fasta"
 }
 
-#############################################Input Control#######################################################
-do_splits=false; do_extract=false
-while getopts ":seh" opt; do
+# --- Argument Parsing ---
+do_splits=false
+do_extract=false
+
+while getopts "seh" opt; do
    case ${opt} in
-      s)
-         do_splits=true
-         ;;
-      e)
-         do_extract=true
-         ;;
-      h)
-         Help
-         exit 0
-         ;;
-     \?)
-         echo "Invalid option: $OPTARG" 1>&2
-         Help
-         exit 1
-         ;;
+      s) do_splits=true ;;
+      e) do_extract=true ;;
+      h) Help; exit 0 ;;
+      *) Help; exit 1 ;;
    esac
 done
-
 shift $((OPTIND - 1))
 
-re='^[0-9]+$'
-if [ $# -lt 2 ]; then
-   echo "At least two arguments must be provided"
-   Help
-   exit 1
-elif [ ! -f "$1" ]; then
-   echo "\"$1\" not found."
-   Help
-   exit 1 
-elif ! [[ $2 =~ $re ]]; then
-   echo "\"$2\" is not an integer."
-   Help
-   exit 1
+# Validation
+if [[ $# -lt 2 ]]; then
+    echo "❌ Error: Missing arguments."
+    Help; exit 1
 fi
 
-if [[ $do_splits == true && $do_extract == true ]]; then
-   # shellcheck disable=SC2086
-   split_file "$1" "$2" "$3"
-   extract_seqs "$1" "$2"
-elif [[ $do_splits ==  false && $do_extract == false ]]; then
-   echo "Either of the flags -s and -e is required."
-   Help
-   exit 1
-elif [[ $do_splits == true ]]; then
-  split_file "$1" "$2"
+input_file="$1"
+value="$2"
+custom_prefix="${3:-}"
+
+if [[ ! -f "$input_file" ]]; then echo "❌ Error: File $input_file not found."; exit 1; fi
+if ! [[ "$value" =~ ^[0-9]+$ ]]; then echo "❌ Error: $value is not a valid number."; exit 1; fi
+
+# Execution
+if [[ "$do_splits" == true ]]; then
+    split_file "$input_file" "$value" "$custom_prefix"
+elif [[ "$do_extract" == true ]]; then
+    extract_seqs "$input_file" "$value" "$custom_prefix"
 else
-  extract_seqs "$1" "$2"
+    echo "❌ Error: You must specify -s (split) or -e (extract)."
+    Help; exit 1
 fi
-#################################################################################################################
