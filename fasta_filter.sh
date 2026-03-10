5f07e52a0cfdf3beca73ceaf3c620802abdafe1b
mikael.leijon <mikael.leijon@gmail.com>
2026-03-10 09:52:16 +0100

    updates

--- fasta_filter.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed '/>/a %' $1|sed -z 's/\n//g'|sed 's/>/\n>/g'|grep -v '%.*[XNYRKMWSBHVD].*'|sed 's/%/\n/g'
--- runspades.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-FILES="/storage/micke/fsk/ipn/*1_001.fastq.gz"
-for f in $FILES; do
-name=${f##*/ipn/}
-outdir=/storage/micke/fsk/ipn/spades/${name%%_L001_*}
-wait;spades.py -o $outdir -1 $f -2 ${f/1_001.fastq.gz/2_001.fastq.gz} -t 72 -m 350; sleep 1m;sync;
-done
